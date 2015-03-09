011484fdf26d41e4819dcedda5fee868987b4c6a
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2015-03-09 22:11:44 +0000

    Terminator for the loop.

--- Task Sheet 5 - Maze/phase1.move.c
+++ Task Sheet 5 - Maze/phase1.move.c
@@ -8,18 +8,13 @@ void goToXY(struct XY destination)
   double yDifference;
   double requiredAngleChange;
   double remainingDistance;
-  while (size > 0) //TODO
+  while (remainingDistance < 20) // value to change
   {
     updateRobotPosition(&bearing, &xPos, &yPos); //TODO
   	xDifference = destination.x - xPos;
     yDifference = destination.y - yPos;
     requiredAngleChange = atan2(xDifference,yDifference) - bearing;
     remainingDistance = sqrt(xDifference*xDifference + yDifference*yDifference);
-
-    if(remainingDistance < 20)
-    {
-    	// TODO
-    }
 
     if(fabs(requiredAngleChange) > 1.2) // for sharp turns, in practice only for turning back at the beginning
     {
