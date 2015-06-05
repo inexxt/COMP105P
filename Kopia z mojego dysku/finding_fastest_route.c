26fe9b4021dd27065f25f1c5a9e01ef97f0fb745
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2015-06-05 21:49:16 +0100

    Added timer (because laziness)

--- Kopia z mojego dysku/finding_fastest_route.c
+++ Kopia z mojego dysku/finding_fastest_route.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 
-#define DEBUG 1
+#define DEBUG 0
 
 #define WID 4
 #define HEI 4
@@ -336,7 +337,7 @@ position BFS(position p)
         BFSQueue = BFSQueue->next;
         move last = cameFrom[p.x][p.y];
         
-        k += DEBUG //DEBUG LINE
+        k += DEBUG; //DEBUG LINE
         int i = 0;
         for(i=0; i<5; i++)
         {
@@ -370,11 +371,16 @@ position BFS(position p)
 
 positionQ* solve()
 {
+    clock_t t = clock();
     mapPreprocessing();
 //     printMap();            
     cameFrom[STARTPOSITION.x][STARTPOSITION.y] = (move){1,1};
     position finish = BFS(STARTPOSITION);
     printf("FINISH %d %d\n", finish.x, finish.y); 
+
+    t = clock() - t;
+    printf("Time taken in seconds: %f\n", ((float)t)/CLOCKS_PER_SEC);
+
     int k;
     scanf("%d", &k);
     printPath(finish);
@@ -400,9 +406,7 @@ int main()
     maze[3][1] = (Sector){1, 0, 0, 1, 1};
     maze[3][2] = (Sector){1, 1, 0, 1, 1};
     maze[3][3] = (Sector){1, 1, 0, 1, 1};
-    
     solve();
     printMapFinal();
     return 0;
 }
-
