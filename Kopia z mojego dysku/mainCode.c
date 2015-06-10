241aeec08e95c8147b38f7e431edcf23684f9b89
inexxt <jac.karwowski@gmail.com>
2015-06-10 14:05:34 +0200

    everything

--- Kopia z mojego dysku/mainCode.c
+++ Kopia z mojego dysku/mainCode.c
@@ -5,6 +5,9 @@
 #include "recordPosition.h"
 #include "basicMovement.h"
 
+
+#define CUTTING 1
+
 /*defined as extern globals*/
 double targetWallReadings;
 double sensorDifference;
@@ -153,81 +156,112 @@ int main()
    
 //     optimizeQueue(); //DANGER test it!!
 
-    int i = 0;
-    for(i=0; i<length; i++)
+   
+   
+//    ------------------------- CUTTING
+    if(CUTTING)
     {
-        qP[i] = (XY){0, 0};
-    }
-    
-    length = 0;
-    XY help;
-    while(!isEmpty(a))
-    {
-        help = popFront(&a);
-        qP[length] = (XY){.x = help.x*60, help.y*60}; 
-        length++;
-    }
-    
-    int px, py, cx, cy, ax, ay, bx, by;
-
-    printfMyQueue();
-    
-    int k = 1;
-    
-    XY b1, b2;
-    
-    length++;
-    
-    for(k=0; k<length - 2; k++)
-    {
-        ax = qP[k].x;
-        ay = qP[k].y;
         
-        cx = qP[k+2].x;
-        cy = qP[k+2].y;
-        
-        printf("first %d %d twoAfter %d %d difference %d %d\n", ax, ay, cx, cy, abs(ax - cx), abs(ay - cy));
-        if(abs(ax - cx) == 60 && abs(ay - cy) == 60 && ax % 60 == 0 && ay % 60 == 0 && cy % 60 == 0 && cx % 60 == 0) //we can cut corner
+        int i = 0;
+        for(i=0; i<length; i++)
         {
-            px = qP[k+1].x;
-            py = qP[k+1].y;
+            qP[i] = (XY){0, 0};
+        }
+        
+        length = 0;
+        XY help;
+        while(!isEmpty(a))
+        {
+            help = popFront(&a);
+            qP[length] = (XY){.x = help.x*60, help.y*60}; 
+            length++;
+        }
+        
+        int px, py, cx, cy, ax, ay, bx, by;
+
+        printfMyQueue();
+        
+        int k = 1;
+        
+        XY b1, b2;
+        
+        length++;
+        
+        for(k=0; k<length - 2; k++)
+        {
+            ax = qP[k].x;
+            ay = qP[k].y;
             
-            deleteFromN(k+1);
+            cx = qP[k+2].x;
+            cy = qP[k+2].y;
             
-            bx = (ax+cx)/2;
-            by = (ay+cy)/2;
-            
-            if(py != ax)
+            printf("first %d %d twoAfter %d %d difference %d %d\n", ax, ay, cx, cy, abs(ax - cx), abs(ay - cy));
+            if(abs(ax - cx) == 60 && abs(ay - cy) == 60 && ax % 60 == 0 && ay % 60 == 0 && cy % 60 == 0 && cx % 60 == 0) //we can cut corner
             {
-                b1.x = ax;
-                b1.y = by;
+                px = qP[k+1].x;
+                py = qP[k+1].y;
                 
-                b2.x = bx;
-                b2.y = cy;
+                deleteFromN(k+1);
+                
+                bx = (ax+cx)/2;
+                by = (ay+cy)/2;
+                
+                if(py != ax)
+                {
+                    b1.x = ax;
+                    b1.y = by;
+                    
+                    b2.x = bx;
+                    b2.y = cy;
+                }
+                else
+                {
+                    
+                    b1.x = bx;
+                    b1.y = ay;
+            
+                    b2.x = cx;
+                    b2.y = by;
+                }
+                b1.x += (b1.x-bx)/3;
+                b1.y += (b1.y-by)/3;
+            
+                b2.x += (b2.x-bx)/3;
+                b2.y += (b2.y-by)/3;
+                    
+                putInN(k+1, b2);
+                putInN(k+1, b1);
             }
-            else
+        }
+        
+        length--;
+        
+        printfMyQueue();
+        
+        for(i=0; i<=length; i++)
+        {
+            if(qP[i].y != 0 || i < 2)
             {
-                b1.x = bx;
-                b1.y = ay;
-                
-                b2.x = cx;
-                b2.y = by;
+                rotateTowardsTarget(qP[i].x, qP[i].y, 2);
+                moveToTarget(qP[i].x, qP[i].y, 2);
             }
-                
-            putInN(k+1, b2);
-            putInN(k+1, b1);
         }
     }
+    //    ------------------------- CUTTING
     
-    length--;
     
-    printfMyQueue();
     
-    for(i=0; i<length; i++)
+    
+//    ------------------------- NO CUTTING 
+    if(!CUTTING)
     {
-        rotateTowardsTarget(qP[i].x, qP[i].y, 2);
-        moveToTarget(qP[i].x, qP[i].y, 2);
+        while(!isEmpty(a))
+        {
+            goToXY(popFront(&a), 2);
+        }
     }
+
+//    ------------------------- NO CUTTING
 
     //Solution 2
     // go(); 
